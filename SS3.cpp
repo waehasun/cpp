78ad873959991e2977a4827a1356a899d1fd80f0
Waehasun <sanyugi@gmail.com>
2025-10-07 21:27:53 +0700

    Update SS3.cpp

--- SS3.cpp
+++ SS3.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Student {
+    string name;
+    float score;
+};
+
+int main() {
+    Student s[5];
+    for (int i = 0; i < 5; i++) {
+        cout << "ชื่อนักเรียนคนที่ " << i+1 << ": ";
+        cin >> s[i].name;
+        cout << "คะแนน: ";
+        cin >> s[i].score;
+    }
+
+    string search;
+    cout << "\nค้นหาชื่อนักเรียน: ";
+    cin >> search;
+
+    bool found = false;
+    for (int i = 0; i < 5; i++) {
+        if (s[i].name == search) {
+            cout << "คะแนนของ " << s[i].name << " คือ " << s[i].score << endl;
+            found = true;
+            break;
+        }
+    }
+
+    if (!found)
+        cout << "ไม่พบนักเรียนในระบบ\n";
+
+    return 0;
+}
 
